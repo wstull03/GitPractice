747a5b9c9fcd6c0bc2a32dc636fe1824d0acd8da
Your Name <you@example.com>
2016-03-02 10:15:45 -0500

    setting up repo

---
+++ ConsoleApplicationGitPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationGitPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 3;
+            int b = 4;
+            Console.WriteLine(a + b);
+
+            Console.Read();
+
+
+        }
+    }
+}
